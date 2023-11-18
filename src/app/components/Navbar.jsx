b32bdde9c21c672f533cb2d7f487b611686338a5
Kartikeya Saini <kudlu2004@gmail.com>
2023-11-18 07:15:27 +0530

    Made routes public and accessible for all

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { BiLogoGmail } from "react-icons/bi";
+import { BiCross, BiLogoGmail } from "react-icons/bi";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { FiDownload } from "react-icons/fi";
 import CustomSizeSkeleton from "./CustomSizeSkeleton";
@@ -23,6 +23,8 @@ import { usePathname } from "next/navigation";
 import { Skeleton } from "./ui/skeleton";
 import PortfolioNotCreated from "@/app/utilpages/PortfolioNotCreated";
 import userNotFoundData from "@/app/utilpages/UserNotFoundApi";
+import { IoIosClose } from "react-icons/io";
+import { Separator } from "./ui/separator";
 
 const NavData = {
   name: "Kartikeya Saini",
@@ -37,8 +39,9 @@ const NavData = {
 function Navbar({ showProfile, data }) {
   const [loading, setLoading] = useState(true);
   const [showCode, setShowCode] = useState(false);
+  const [display, setDisplay] = useState(true);
   data = data || userNotFoundData;
-  
+
   useEffect(() => {
     data = data || userNotFoundData;
     setLoading(false);
@@ -54,6 +57,29 @@ function Navbar({ showProfile, data }) {
 
   return (
     <nav className="border-b-2">
+      {!showProfile && (
+        <div
+          className={`${
+            display ? "" : "hidden"
+          } container flex flex-col w-full justify-between p-2`}
+        >
+          <div className="flex justify-between items-center pb-2">
+            <div className="left flex gap-4 flex-wrap">
+              <Link href="/my-portfolio">My Portfolio</Link>
+              <Link href="/my-messages">My Messages</Link>
+              <Link href="/add-blog">Add Blog</Link>
+            </div>
+
+            <IoIosClose
+              size={25}
+              onClick={() => {
+                setDisplay(false);
+              }}
+            />
+          </div>
+          <Separator />
+        </div>
+      )}
       <div className="container flex gap-5 justify-between px-4 py-2 items-center hover: bg-none outline-none">
         <div className="relative">
           {showProfile ? (
@@ -129,7 +155,10 @@ function Navbar({ showProfile, data }) {
           )}
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
+          <Link href={`/${sessionStorage.getItem("username")}/blogs`}>
+            Blogs
+          </Link>
           <ModeToggle />
         </div>
       </div>
