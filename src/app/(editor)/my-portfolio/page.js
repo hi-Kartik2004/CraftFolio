3c06fb2b69bebd4d126a46e5774eb6efb820223f
Kartikeya Saini <kudlu2004@gmail.com>
2023-11-18 18:53:50 +0530

    My Portfolio route security added

--- src/app/(editor)/my-portfolio/page.js
+++ src/app/(editor)/my-portfolio/page.js
@@ -6,6 +6,7 @@ import EditPortfolio from "@/app/components/EditPortfolio";
 import { Skeleton } from "@/app/components/ui/skeleton";
 import Link from "next/link";
 import { BiArrowBack } from "react-icons/bi";
+import { ClerkLoading, SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 
 function EditPortfolioPage() {
   const [loading, setLoading] = useState(true);
@@ -25,28 +26,40 @@ function EditPortfolioPage() {
 
   return (
     <section className="container">
-      <Button variant="link">
-        <Link href="/" className="my-4 flex gap-2 items-center">
-          <BiArrowBack /> Home
-        </Link>
-      </Button>
-      <div>
-        <SectionHeading
-          title="Edit your"
-          badge="Your area"
-          gradientText="Portfolio"
-        />
-        <p className="text-muted-foreground">
-          <Button variant="link" className="p-0 pr-2">
-            How to make your portfolio?
-          </Button>
-          Not Sure how to get started or are you new to coding, don't worry what
-          you are about to do isn't coding, but we suggest you watch this 2min
-          video for better understanding.{" "}
-        </p>
-      </div>
+      <SignedOut>
+        <ClerkLoading>
+          <div className="flex justify-center items-center min-h-[80vh]">
+            <Skeleton className="w-[280px] h-[280px] rounded-lg" />
+          </div>
+        </ClerkLoading>
+        <div className="flex justify-center items-center min-h-[80vh]">
+          <SignIn afterSignInUrl="/my-messages" />
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Button variant="link">
+          <Link href="/" className="my-4 flex gap-2 items-center">
+            <BiArrowBack /> Home
+          </Link>
+        </Button>
+        <div>
+          <SectionHeading
+            title="Edit your"
+            badge="Your area"
+            gradientText="Portfolio"
+          />
+          <p className="text-muted-foreground">
+            <Button variant="link" className="p-0 pr-2">
+              How to make your portfolio?
+            </Button>
+            Not Sure how to get started or are you new to coding, don't worry
+            what you are about to do isn't coding, but we suggest you watch this
+            2min video for better understanding.{" "}
+          </p>
+        </div>
 
-      <EditPortfolio />
+        <EditPortfolio />
+      </SignedIn>
     </section>
   );
 }
