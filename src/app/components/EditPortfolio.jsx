0313d8ce2f849f8d2d8cadd1c460473c1a621732
Kartikeya <kudlu2004@gmail.com>
2023-12-05 14:35:03 +0530

    Added new gpt link

--- src/app/components/EditPortfolio.jsx
+++ src/app/components/EditPortfolio.jsx
@@ -200,7 +200,7 @@ function EditPortfolio() {
             </AlertDialogTrigger>
             <AlertDialogContent className="h-[90vh]">
               <iframe
-                src="https://app.chatgptbuilder.io/webchat/?p=1232272&ref=1701706101615"
+                src="https://app.chatgptbuilder.io/webchat/?p=1232272&ref=1701705297211"
                 width="100%"
                 className="h-[500px] md:h-[575px]"
               ></iframe>
